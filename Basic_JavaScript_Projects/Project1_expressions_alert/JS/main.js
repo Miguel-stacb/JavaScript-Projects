3c77ba4b23e898d27eaddb624b0bee45073401f9
Miguel-stacb <64321960+Miguel-stacb@users.noreply.github.com>
2024-04-30 12:38:29 -0500

    Step 64 Basic JS projects assignment

---
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,29 @@
+alert("Hello world"); // alert method example
+document.write("Hello world! <br>");// document write method example
+var A = "This is a string";// Varaible A string assignment
+document.write(A,"<br>");
+document.write("Lisa told Bart, \"Knock it off, Bart! Or I'll tell dad!.\" \"Eat my shorts!\" Bart responded. <br>");
+document.write("\"Be who you are and say what you feel,"+" because those who mind don\'t matter and those who matter don\'t mind.\"<br>"
++"-Dr.Seuss <br>") // Concatenate example
+
+var B = "Concatenated " + "String <br>"; // Expression concateneted
+document.write(B); // Print concatenated expression
+var Family = "The Arezzinis", Dad = "Jeremiah", Mom = "Hermoine", Daughter = "Penny", Son = "Zorro";// Multiple variable assignment
+document.write(Dad,"<br>"); // Print dad variable
+
+var D = 3 + 3;// Expression example
+document.write(D,"<br>");// Print expression
+
+var sent1 = "This is the beginning of the string\n<br>";// Var sent1 string assignment
+var sent2 = "and this is the end of the string";//Var sent2 string assignment
+
+var E = sent1 + sent2;//Expression for sent1 and sent2 concatenation
+
+document.write(E,"<br>");// Print concatenated sent1 and sent 2 variables
+
+var F = 12;// var F assignment
+var G = 8;// var G assignment
+
+var R = F * G;// Example of expression using var F and G
+
+document.write(R, "<br>"); // Print expression assigned to variable R
