dd678836e95442e117923b13da9ac004fb76f394
Miguel-stacb <64321960+Miguel-stacb@users.noreply.github.com>
2024-04-30 17:53:18 -0500

    Functions projects

---
+++ Basic_JavaScript_Projects/Project2_functions/JS/main.js
@@ -0,0 +1,32 @@
+function displayVariables(){
+
+    //Asign values to two variables
+
+    var one = "John";
+    var two = "Smith";
+
+    //Concatenate the variables
+
+    var fullName = one + " " + two;
+
+    //Get the element with the id "Button-Text" and set its value
+
+    document.getElementById("Button-Text").innerHTML=fullName;
+
+     //Get the element with the id "formField" and set its value
+
+}
+    
+function myFunction(){
+
+    //Concatenate two text strings
+
+    var sentence = "I am learning";
+    sentence += " a lot from this book!";
+
+  //Get the element with the id "Concatenate" and set its value
+
+    document.getElementById("Concatenate").innerHTML = sentence;
+
+}
+
