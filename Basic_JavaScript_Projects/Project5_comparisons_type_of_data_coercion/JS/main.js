f1425487091bdf961c75c8705b0f52707987902f
Miguel-stacb <64321960+Miguel-stacb@users.noreply.github.com>
2024-05-03 13:24:13 -0500

    Project5

--- Basic_JavaScript_Projects/Project5_comparisons_type_of_data_coercion/JS/main.js
+++ Basic_JavaScript_Projects/Project5_comparisons_type_of_data_coercion/JS/main.js
@@ -1,12 +1,12 @@
-document.write(typeof "Word","<br>");
+document.write(typeof "Word","<br>"); // Example of typeof 
 
-document.write(typeof 3,"<br>");
+document.write(typeof 3,"<br>"); // Example of typeof
 
-document.write("10" + 5);
+document.write("10" + 5,"<br>"); // Concatenate diferent type of data number and string combined
 
 function not_a_Number() {
 
-    document.getElementById("nNum").innerHTML = 0/0;    
+    document.getElementById("nNum").innerHTML = 0/0;    //Example of math expression NaN
 }
 
 function true_Function(){
@@ -30,5 +30,49 @@ function ninfinity_Number(){
 
     document.getElementById("nInfinity").innerHTML = (-3E310);
     
+}
+
+document.write(10>2,"<br>");
+
+document.write(10<2,"<br>");
+
+console.log(2 + 2);
+
+console.log(10<2);
+
+document.write(10 == 10,"<br>"); //Example of == use
+
+document.write(3 == 11,"<br>");
+
+x = 10;
+y = 10;
+document.write(x === y,"<br>");//Example of === use
+
+a = 10;
+b = "doce";
+document.write(a === b,"<br>");
+
+i = 20;
+j = "20";
+document.write(i === j,"<br>");
+
+k = 15;
+l = 25;
+document.write(k === l,"<br>");
+
+document.write(5>2 && 10>4,"<br>"); // Example of && use
+document.write(5>10 && 10>4,"<br>");
+document.write(5>10 || 10>4,"<br>");//Example of || use
+document.write(5>10 || 10>20,"<br>");
+
+function not_Function() {
+
+    document.getElementById("Not").innerHTML = !(20 > 10); // Example of ! use
+
+}
+
+function dnot_Function(){
+
+    document.getElementById("dNot").innerHTML = !(5 > 10);
 }
 
