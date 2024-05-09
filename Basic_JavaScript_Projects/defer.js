e4da0dbf983a46cb333ce906bfc8fae719b5714f
Miguel-stacb <64321960+Miguel-stacb@users.noreply.github.com>
2024-05-09 08:04:12 -0500

    Defer, debug, async codee examples

---
+++ Basic_JavaScript_Projects/defer.js
@@ -0,0 +1,10 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Tu código aquí
+    // script.js
+function greetUser() {
+    const greetingElement = document.getElementById("greeting");
+    greetingElement.textContent = "Hello, World!";
+}
+    greetUser();
+});
